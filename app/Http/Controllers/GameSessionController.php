c150905fcf1720a3d6eb19b0d12c7b114a0086ba
Daniel Radescu <daniel.radescu@gmail.com>
2025-11-02 19:27:51 +0200

    interact with game session

--- app/Http/Controllers/GameSessionController.php
+++ app/Http/Controllers/GameSessionController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Enums\NotificationType;
 use App\Enums\RegistrationStatus;
 use App\Models\GameSession;
+use App\Models\Notification;
 use App\Models\Registration;
 use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class GameSessionController extends Controller
 {
@@ -27,8 +29,17 @@ class GameSessionController extends Controller
 
     public function show($uuid)
     {
+        $gameSession = GameSession::where('uuid', $uuid)->firstOrFail();
+        $myRegistration = Registration::where('user_id', Auth::user()->id)
+            ->where('game_session_id', $gameSession->id)->first();
+        $allRegistrations = Registration::where('game_session_id', $gameSession->id)
+            ->with('user')
+            ->get();
+
         return view('game-session.detail', [
             'gameSession' => GameSession::where('uuid', $uuid)->firstOrFail(),
+            'registrationStatus' => $myRegistration ? $myRegistration->status : null,
+            'registrations' => $allRegistrations,
         ]);
     }
 
@@ -36,34 +47,42 @@ class GameSessionController extends Controller
     {
         $gameSession = GameSession::where('uuid', $uuid)->firstOrFail();
 
+
         Registration::where('user_id', Auth::user()->id)
             ->where('game_session_id', $gameSession->id)
             ->delete();
 
         $registrationStatus = null;
-        $notification = null;
+        $notificationStatus = null;
         switch ($request->get('action')) {
-            case 'yes':
+            case 'confirm':
                 $registrationStatus = RegistrationStatus::Confirmed;
+                $notificationStatus = NotificationType::Confirmed;
                 break;
-            case '1day':
+            case '2day':
                 $registrationStatus = RegistrationStatus::Interested;
-                $notification = NotificationType::
+                $notificationStatus = NotificationType::ConfirmationReminder2Days;
                 break;
-            case  '2day':
-                $registrationStatus = RegistrationStatus::Interested;
+            case 'decline':
+                $registrationStatus = RegistrationStatus::Declined;
                 break;
             default:
                 abort(404);
         }
 
-        Registration::create([
+        $registration = Registration::create([
             'user_id' => Auth::user()->id,
             'game_session_id' => $gameSession->id,
             'status' => $registrationStatus,
         ]);
+        if ($notificationStatus) {
+            Notification::create([
+                'registration_id' => $registration->id,
+                'type' => $notificationStatus->value,
+            ]);
+        }
 
-        dd($request->all());
+        return Redirect::route('show.game-session', $uuid);
     }
 
     public function create()
--- resources/views/game-session/detail.blade.php
+++ resources/views/game-session/detail.blade.php
@@ -17,7 +17,7 @@
                             <div><strong>Starting at:</strong> {{ $gameSession->start_at->format('H:i') }}</div>
                             <div><strong>Location:</strong> {{ $gameSession->location ?? 'To be decided' }}</div>
                             <div><strong>Expected:</strong> {{ $gameSession->min_players }}–{{ $gameSession->max_players }} players</div>
-                            <div><strong>Complexity</strong>
+                            <div><strong>Average complexity</strong>
                                 <span class="inline-flex items-center gap-1">
                                     @for ($i = 0; $i < round($gameSession->complexity); $i++)
                                         <span class="text-purple-400 text-lg">🧠</span>
@@ -29,16 +29,47 @@
 
                             <div>{{$gameSession->description}}</div>
                             <div><strong>Status:</strong> {{$gameSession->type->label()}}</div>
-                            <div><strong>People now:</strong> <img class="h-10 w-10 rounded-full object-cover" src="{{ asset(Auth::user()->getPhotoURL()) }}" alt="Profile Image"> <img class="h-10 w-10 rounded-full object-cover" src="{{ asset(Auth::user()->getPhotoURL()) }}" alt="Profile Image"> <img class="h-10 w-10 rounded-full object-cover" src="{{ asset(Auth::user()->getPhotoURL()) }}" alt="Profile Image"></div>
-                            <div><strong></strong> </div>
+                            <div><strong>People confirmed:</strong>
+
+                                @php
+                                    $confirmedRegistrations = $registrations->where('status', \App\Enums\RegistrationStatus::Confirmed->value)->all()
+                                @endphp
+
+                                @if($confirmedRegistrations)
+                                    @foreach($confirmedRegistrations as $registration)
+                                        <img class="inline-flex h-5 w-5 rounded-full object-cover" src="{{ asset($registration->user->getPhotoURL()) }}" alt="Profile Image">
+                                    @endforeach
+                                @else
+                                    - None -
+                                @endif
+                            </div>
+                            <div><strong>Other people interested:</strong>
+                                @php
+                                    $interestedRegistrations = $registrations->where('status', \App\Enums\RegistrationStatus::Interested->value)->all()
+                                @endphp
+                                @if($interestedRegistrations)
+                                    @foreach($interestedRegistrations as $registration)
+                                        <img class="inline-flex h-5 w-5 rounded-full object-cover" src="{{ asset($registration->user->getPhotoURL()) }}" alt="Profile Image">
+                                    @endforeach
+                                @else
+                                    - None -
+                                @endif
+                            </div>
+                            <div><strong>Can't make it: X{{$registrations->where('status', \App\Enums\RegistrationStatus::Declined->value)->count()}}</strong></div>
+
                             <div><strong></strong> </div>
                             <div><strong></strong> </div>
                             <div><strong></strong> </div>
                             <div><strong></strong> </div>
                             <div><strong></strong> </div>
                         </div>
-
+                        <hr class="m-2"/>
                         <div class="mt-4">
+                            @if($registrationStatus)
+                                <p>You - {{$registrationStatus->label()}}</p>
+                                <p>Have you changed you mind?</p>
+                            @endif
+
                             <form action="{{ route('game-session.handle', $gameSession->uuid) }}" method="POST" class="max-w-md space-y-3">
                                 @csrf
 
@@ -49,19 +80,27 @@
                                     🎯 Count me in — reserve my seat!
                                 </button>
 
-                                <button type="submit" name="action" value="1day"
+                                <button type="submit" name="action" value="2day"
                                         class="w-full px-4 py-2 rounded-md border border-emerald-200 bg-emerald-50 text-emerald-800
                                            hover:bg-emerald-100 focus:outline-none focus:ring-2 focus:ring-emerald-200
                                            dark:border-emerald-900 dark:bg-emerald-900/20 dark:text-emerald-300 dark:hover:bg-emerald-900/30">
-                                    🕓 Remind me a day before — I might join.
-                                </button>
-
-                                <button type="submit" name="action" value="2days"
-                                        class="w-full px-4 py-2 rounded-md border border-amber-200 bg-amber-50 text-amber-800
-                                           hover:bg-amber-100 focus:outline-none focus:ring-2 focus:ring-amber-200
-                                           dark:border-amber-900 dark:bg-amber-900/20 dark:text-amber-300 dark:hover:bg-amber-900/30">
                                     ⏰ Remind me two days before — still deciding.
                                 </button>
+                                @if($registrationStatus && ($registrationStatus->value != \App\Enums\RegistrationStatus::Declined->value))
+                                    <button type="submit" name="action" value="decline"
+                                            class="w-full px-4 py-2 rounded-md border border-gray-300 bg-gray-50 text-gray-700
+                                             hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300
+                                             dark:border-gray-700 dark:bg-gray-800/30 dark:text-gray-300 dark:hover:bg-gray-800/50">
+                                        🚫 I can’t make it
+                                    </button>
+                                @endif
+                                <p class="px-4"><i>
+                                        By interacting with this game session (for example by showing interest, or confirming attendance),
+                                        you agree to receive related updates and notifications about the event and its status
+                                        from now until the session has concluded.
+                                        These may include reminders, schedule adjustments, and important announcements
+                                        necessary for participation and coordination.</i>
+                                </p>
                             </form>
                         </div>
                     </div>
